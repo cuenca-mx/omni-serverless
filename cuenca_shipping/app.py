97250839c7110ef1158d338b35ddfd3cbdabd53e
keryc Diaz <keryc@cuenca.com>
2019-07-16 02:23:15 -0500

    fixes comment and interior, deploy

--- cuenca_shipping/app.py
+++ cuenca_shipping/app.py
@@ -2,8 +2,11 @@ import datetime
 import json
 import os
 
+MONGO_URI = os.environ['MONGO_URI']
+CORS_ORIGIN = os.environ['CORS_ORIGIN']
+
 from mongoengine import *
-connect('omni', host=os.environ['MONGO_URI'])
+connect('omni', host=MONGO_URI)
 
 class CustomerForCard(DynamicDocument):
     pass
@@ -16,7 +19,7 @@ def respond(err, res=None):
             'Content-Type': 'application/json',
             "Access-Control-Allow-Headers": "Content-Type",
             "Access-Control-Allow-Methods": "OPTIONS,POST",
-            "Access-Control-Allow-Origin": os.environ['CORS_ORIGIN']
+            "Access-Control-Allow-Origin": CORS_ORIGIN
         },
     }
 
@@ -29,8 +32,13 @@ def lambda_handler(event, context):
                 entered_address=False
             )
             if customer_for_card:
+                geocoding_gmaps = payload['geocoding_gmaps']
+                if 'comment' in payload and payload['comment']:
+                    geocoding_gmaps['comment'] = payload['comment']
+                if 'internal_number' in payload and payload['internal_number']:
+                    geocoding_gmaps['internal_number'] = payload['internal_number']
                 customer_for_card.update(**dict(
-                    geocoding_gmaps=payload['geocoding_gmaps'],
+                    geocoding_gmaps=geocoding_gmaps,
                     entered_address=True,
                     updated_at=datetime.datetime.utcnow()
                 ))
